187b041edeb0d9eafc1b840ad996c7e13303af34
lvyang <710162219@qq.com>
2020-09-15 17:00:46 +0800

    hello spring boot

---
+++ src/main/java/com/test/HelloWorldMainApplication.java
@@ -0,0 +1,18 @@
+package com.test;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * springBootAppilication 来标注一个主程序，说明这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+        //Spring应用起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
